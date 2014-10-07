cc014e56fa9a709ea41187f78ee5b4ca9190546f
Fabi_Laptop <fabian-todt@gmx.de>
2014-10-07 11:04:07 +0200

    had to add some files to commit, everythings the same
    Many things done:
    - MainActivity scans the smartphone services for right ble support and forces to turn bluetooth on.
    -> When everything is okay: Scans for near devices (with rssi) and finds broadcasting ones.
    -> To reach that I had to remove the whole Tab-Environment and replace it with a List-Environment,
    that marks known bottles green (else they are black)
    -> but there is no saving function for the bottle, so that's something important to do
    --> save it in file, but remember function of geo-location, that will maybe clarify exactly the
    last position of earlier users,	so we maybe need hashing or an internal	passphrase
    
    - with onClick on Item in list you will get to the BottleDetails, where	you will get directly connected	with the device
    -> some	information are	listed there (mac, name, provisional id)
    -> and an expandable list, that	lists known services of	the device (at the moment, there is only the bottlemail	service
    that I could interpret as useful- so I hardcoded its characteristic that sends "Bottlemail. Hello..." to BMAIL_HELLO)
    -> you can disconnect (fast) and reconnect again (very slow, up to 20seconds, be patient)

---
+++ v2/src/unused/BleHandler.java
@@ -0,0 +1,172 @@
+package unused;
+
+
+public class BleHandler {
+
+	//TODO just copied from Version1, added only needed stuff to compile
+	
+	// debugging
+	private static final String TAG = "BleHandler";
+
+//	private BtConnectionTask mBtCurentConnection;
+//
+//	private static BleHandler mBleHandler = new BleHandler();
+//	private BluetoothAdapter mBleAdapter;	
+//	private BleHandler(){
+//	
+//		Log.e( TAG, "+++++ construct BtHandler +++++");
+//		mBleAdapter = BluetoothAdapter.getDefaultAdapter();	
+//	}
+//	
+//	
+//	public static BleHandler getInstance() {
+//		
+//		Log.d( TAG, "+++ getInstance +++");
+//		return mBleHandler;
+//	}
+//
+//	//scanning for devices
+//	public void startDiscovery() throws Exception {
+//		
+//		if (mBleAdapter.isEnabled()) {
+//			mBleAdapter.startDiscovery();
+//		}
+//		else{
+//			throw new Exception();
+//		}
+//	}
+//	
+//	//stop scanning for devices
+//	public void cancelDiscovery(){
+//		
+//		if(mBleAdapter != null){
+//			if(mBleAdapter.isDiscovering())
+//				mBleAdapter.cancelDiscovery();
+//		}
+//	}
+	
+	
+//	//holt ID der Flasche
+//	public void getBottleID(Bottle bottle, final MessageCallback btlDetailsCB){
+//			
+//		/*Log.d(TAG, "+++++ getBottleID +++++");
+//		
+//		BtClientTask clientTask = startBtClientTask(bottle);
+//		
+//		final ConnectionCallback conCB = createConnectionCallback(btlDetailsCB);
+//       
+//		//Befehl erstellen
+//		
+//		clientTask.execute(new BtConnectionEstablishedCallback() {
+//               @Override
+//               public void connectionEstablished(BluetoothSocket socket) {
+//               	mBtCurentConnection = new BtConnectionTask(socket, conCB); 
+//               	mBtCurentConnection.execute();
+//               	//getBottleID
+//               	mBtCurentConnection.send("24000221a5fd");
+//               	
+//               }       
+//       });*/
+//	}
+	
+//	//holt angegebene Anzahl an Nachrichten von Flasche 
+//	public void getMessages(final Bottle bottle, int numberOfMessages, final MessageCallback messageCallback){
+//			
+//		Log.d(TAG, "+++++ getMessages +++++");
+//		
+//		NoMFinishedCallback noMFinishedCallback = new NoMFinishedCallback(){
+//			@Override
+//			public void incomingData(int numberOfMessages){
+//				Log.d(TAG, "numberOfMessages: " + numberOfMessages);
+//				
+//				for(int i = 1; i<= 3; i++){
+//					getMessage(bottle, i, messageCallback);
+//				}
+//			}
+//		};
+		
+//		NoMConnectionReceiver receiver = new NoMConnectionReceiver(noMFinishedCallback);
+//		
+//		getNumberOfMessages(bottle, receiver);
+//		
+//		/*clientTask.execute(new BtConnectionEstablishedCallback() {
+//           @Override
+//           public void connectionEstablished(BluetoothSocket socket) {
+//           	mBtCurentConnection = new BtConnectionTask(socket, conCB); 
+//           	mBtCurentConnection.execute();
+//           	//getMessages
+//           	//mBtCurentConnection.send("240001000021D527", 10);
+//           }
+//		});*/
+//	}
+//	
+//	private void getNumberOfMessages(Bottle bottle, ConnectionReceiver receiver){
+//		
+//		BtClientTask clientTask = startBtClientTask(bottle);
+//		
+//		clientTask.addCommand("2401010000216E3B", receiver);
+//	}
+//	
+//	private void getMessage(Bottle bottle, int id, final MessageCallback messageCallback){
+//		
+//		BtClientTask clientTask = startBtClientTask(bottle);
+//		
+//		BmailConnectionReceiver receiver = new BmailConnectionReceiver(id, messageCallback);
+//		
+//		if(id == 1){
+//			clientTask.addCommand("24010200040000000121590C", receiver);
+//			clientTask.addCommand("2401030008000000010000000021689C", receiver);
+//		}
+//		else if(id == 2){
+//			clientTask.addCommand("24010200040000000221C23E", receiver);
+//			clientTask.addCommand("2401030008000000020000000021A481", receiver);
+//		}
+//		else if(id == 3){
+//			clientTask.addCommand("240102000400000003214B2F", receiver);
+//			clientTask.addCommand("2401030008000000030000000021E08A", receiver);
+//		}
+//		else if(id == 4){
+//			clientTask.addCommand("24010200040000000421F45B", receiver);
+//			clientTask.addCommand("2401030008000000040000000021E08A", receiver);
+//		}
+//	}
+//	
+//	public void sendMessage(Bottle bottle, BMail bMail, MessageCallback composeMsgCB){
+//		
+//		Log.d(TAG, "+++++ send messages +++++");
+//	}
+//	
+//	private BtClientTask mBtClientTask;
+//	
+//	private BtClientTask startBtClientTask(Bottle bottle){
+//		
+//		if(mBtClientTask == null){
+//			
+//			Log.d(TAG, "+++++ startBtClientTask +++++");
+//			
+//			BluetoothDevice btDevice = null;
+//			
+//			if(bottle.getMac() != null)
+//				btDevice = mBtAdapter.getRemoteDevice(bottle.getMac());
+//			if( btDevice == null){
+//				Log.e("getConnectThread", "Kein BluetoothDevice!!");
+//			}
+//			
+//			mBtClientTask = new BtClientTask(btDevice);
+//			mBtClientTask.execute();
+//		}	
+//		
+//		return mBtClientTask;
+//	}
+//	
+//	public void disconnect(){
+//		if(mBtClientTask != null){
+//			mBtClientTask.disconnect();
+//			
+//			mBtClientTask.cancel(true);
+//			mBtClientTask = null;
+//		}
+//		
+//		cancelDiscovery();
+//	}
+}
---
+++ v2/src/unused/LeDeviceListAdapter.java
@@ -0,0 +1,25 @@
+package unused;
+
+import java.util.Vector;
+
+import android.bluetooth.BluetoothDevice;
+
+public class LeDeviceListAdapter {
+
+	private static Vector<BluetoothDevice> bleDevices = new Vector<BluetoothDevice>();
+	
+	public LeDeviceListAdapter() {
+	}
+	
+	public void notifyDataSetChanged() {
+		
+	}
+
+	public void addDevice(BluetoothDevice device) {
+		bleDevices.add(device);
+	}
+	
+	public Vector<BluetoothDevice> getList () {
+		return bleDevices;
+	}
+}
